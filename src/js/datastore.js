edb9ab1ed4e1edc3f308cc6e66636cd6018ea22d
critmcdonald <christian@cmcdonald.com>
2015-12-29 11:28:36 -0600

    pausing place working on detail template

--- src/js/datastore.js
+++ src/js/datastore.js
@@ -4,10 +4,36 @@
 
   // setting up template for detail
   var tpl = _.template(
-    "<h3><%= name %></h3>" +
-    "<h2><%= address %></h2>" +
-    "<p><%= age %></p>"
+    '<div class="clearfix">' +
+    '<h3 class="name"><%= name %></h3>' +
+    '<img class="photo" src="<%= thumb %>" width="50%"/>' +
+    '<h2><%= address %></h2>' +
+    '<p><%= age %></p>'
   );
+
+
+/*
+        <div class="clearfix">
+          <h3 class="name">Name of victim</h3>
+          <img class="photo" src="http://djuiuzgub7yaf.cloudfront.net/D5y20sZMT2FW-fDO1BNKGmnrSag=/200x300/smart/http://media.data.statesman.com/trafficfatal/images/Aguilar_V.jpg" width="50%"/>
+          <p>
+            Hispanic Male, 23.
+            <h5>Date of accident</h5>
+            1/1/2015
+            <h5>Relation</h5>
+            Driver<br>
+            <h5>Accident location</h5>
+            305 S. Congress Ave.
+          </p>          
+        </div>
+
+        <div class="clearfix">
+          <h3>What happened</h3>
+          <p>Description of the accident, if there is one.</p>
+        </div>
+*/
+
+
 
   // defining div for detail
   var $detail = $('#detail');
