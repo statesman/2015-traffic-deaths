24930eb247ce5fefeafc7f4e6c26906876a08985
critmcdonald <christian@cmcdonald.com>
2015-12-30 11:07:57 -0600

    checkpoint at first failed attempt to add table data via json

--- public/index.php
+++ public/index.php
@@ -66,8 +66,8 @@
     </div>
     <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
       <ul class="nav navbar-nav">
-        <li class="active"><a href="./">Home <span class="sr-only">(current)</span></a></li>
-        <li><a href="#">Link</a></li>
+        <li class="active"><a href="./">Interactive <span class="sr-only">(current)</span></a></li>
+        <li><a href="#" target="_blank">Story</a></li>
         <li class="visible-xs small-social"><a target="_blank" href="https://www.facebook.com/sharer.php?u=<?php echo urlencode($meta['url']); ?>"><i class="fa fa-facebook-square"></i></a><a target="_blank" href="https://twitter.com/intent/tweet?url=<?php echo urlencode($meta['url']); ?>&via=<?php print urlencode($meta['twitter']); ?>&text=<?php print urlencode($meta['title']); ?>"><i class="fa fa-twitter"></i></a><a target="_blank" href="https://plus.google.com/share?url=<?php echo urlencode($meta['url']); ?>"><i class="fa fa-google-plus"></i></a></li>
       </ul>
         <ul class="nav navbar-nav navbar-right social hidden-xs">
@@ -93,33 +93,55 @@
       </div>
 
       <div class="col-sm-4 col-xs-12">
-      <div id="detail">
-        <div class="clearfix">
-          <img class="pull-left instructions" src="assets/click-map.png" />
-          <h4>Select a point on the map to see additional details.</h4>
-        </div>
- 
-        <div class="clearfix">
-          <h3 class="name">Name of victim</h3>
-          <img class="photo" src="http://djuiuzgub7yaf.cloudfront.net/D5y20sZMT2FW-fDO1BNKGmnrSag=/200x300/smart/http://media.data.statesman.com/trafficfatal/images/Aguilar_V.jpg" width="50%"/>
-          <p>
-            Hispanic Male, 23.
-            <h5>Date of accident</h5>
-            1/1/2015
-            <h5>Relation</h5>
-            Driver<br>
-            <h5>Accident location</h5>
-            305 S. Congress Ave.
-          </p>          
-        </div>
+        <div id="detail">
+          <div class="clearfix">
+            <img class="pull-left instructions" src="assets/click-map.png" />
+            <h4>Select a point on the map to see additional details.</h4>
+          </div>
+   
+<!--
+          <div class="clearfix">
+            <h3 class="name">Name of victim</h3>
+            <img class="photo" src="http://djuiuzgub7yaf.cloudfront.net/D5y20sZMT2FW-fDO1BNKGmnrSag=/200x300/smart/http://media.data.statesman.com/trafficfatal/images/Aguilar_V.jpg" width="50%"/>
+            <p>
+              Hispanic Male, 23.
+              <h5>Date of accident</h5>
+              1/1/2015
+              <h5>Relation</h5>
+              Driver<br>
+              <h5>Accident location</h5>
+              305 S. Congress Ave.
+            </p>          
+          </div>
 
-        <div class="clearfix">
-          <h3>What happened</h3>
-          <p>Description of the accident, if there is one.</p>
+          <div class="clearfix">
+            <h3>What happened</h3>
+            <p>Description of the accident, if there is one.</p>
+          </div>
+-->
         </div>
-
-
       </div>
+      <div class="col-xs-12">
+        <div class="table-responsive">
+          <table id="detailDataTable" class="table table-condensed table-bordered">
+            <tr>
+              <th>Date</th>
+              <th>Name</th>
+              <th>Race</th>
+              <th>Age</th>
+              <th>Relation</th>
+              <th>Location</th>
+            </tr>
+            <tr>
+              <td>data col 1</td>
+              <td>data col 2</td>
+              <td>data col 3</td>
+              <td>data col 4</td>
+              <td>data col 5</td>
+              <td>data col 6</td>
+            </tr>
+          </table>
+        </div>
       </div>
   </article>
 
--- src/js/datastore.js
+++ src/js/datastore.js
@@ -74,4 +74,28 @@
 
   });
 
+
+ //using exising dataURL function from map.js for data
+ // ann attempt to fill table with json data
+/*
+  $.getJSON(dataUrl, function(data) {
+
+    function drawTable(data) {
+      for (var i = 0; i < data.length; i++) {
+          drawRow(data[i]);
+        }
+    }
+
+    function drawRow(rowData) {
+        var row = $("<tr />");
+        $("#detailDataTable").append(row); //this will append tr element to table... keep its reference for a while since we will add cels into it
+        row.append($("<td>" + rowData.id + "</td>"));
+        row.append($("<td>" + rowData.firstName + "</td>"));
+        row.append($("<td>" + rowData.lastName + "</td>"));
+    }
+
+
+  });
+*/
+
 }(jQuery, _));
