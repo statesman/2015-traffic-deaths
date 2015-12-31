863ed882362a98f867230f8021b4343b70f491eb
critmcdonald <christian@cmcdonald.com>
2015-12-31 00:01:16 -0600

    cleaning up comments

--- src/js/datastore.js
+++ src/js/datastore.js
@@ -22,30 +22,6 @@
 
   );
 
-
-/*
-        <div class="clearfix">
-          <h3 class="name">Name of victim</h3>
-          <img class="photo" src="http://djuiuzgub7yaf.cloudfront.net/D5y20sZMT2FW-fDO1BNKGmnrSag=/200x300/smart/http://media.data.statesman.com/trafficfatal/images/Aguilar_V.jpg" width="50%"/>
-          <p>
-            Hispanic Male, 23.
-            <h5>Date of accident</h5>
-            1/1/2015
-            <h5>Relation</h5>
-            Driver<br>
-            <h5>Accident location</h5>
-            305 S. Congress Ave.
-          </p>          
-        </div>
-
-        <div class="clearfix">
-          <h3>What happened</h3>
-          <p>Description of the accident, if there is one.</p>
-        </div>
-*/
-
-
-
   // defining div for detail
   var $detail = $('#detail');
 
@@ -58,7 +34,7 @@
         id: e.layer.options.id
       });
 
-      // pushing to the templage
+      // pushing to the template
       $detail.html(tpl({
         name: featured.name,
         race: featured.race,
@@ -73,29 +49,5 @@
     });
 
   });
-
-
- //using exising dataURL function from map.js for data
- // ann attempt to fill table with json data
-/*
-  $.getJSON(dataUrl, function(data) {
-
-    function drawTable(data) {
-      for (var i = 0; i < data.length; i++) {
-          drawRow(data[i]);
-        }
-    }
-
-    function drawRow(rowData) {
-        var row = $("<tr />");
-        $("#detailDataTable").append(row); //this will append tr element to table... keep its reference for a while since we will add cels into it
-        row.append($("<td>" + rowData.id + "</td>"));
-        row.append($("<td>" + rowData.firstName + "</td>"));
-        row.append($("<td>" + rowData.lastName + "</td>"));
-    }
-
-
-  });
-*/
 
 }(jQuery, _));
--- src/js/datatable.js
+++ src/js/datatable.js
@@ -1,8 +1,11 @@
 (function($) {
 
+  // setting up datatables module data
+  // link to data below should really come from map.data,
+  // but I don't know how
   $(document).ready(function() {
       $('#detailTable').DataTable( {
-          "ajax": "data-test-table.json",
+          "ajax": "data-test-table.json", // link to the data
           "columns": [
               { "data": "date_time_converted" },
               { "data": "name" },
